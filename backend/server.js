e4d802d9033499fc256c1c90e1a4fb24e60b067f
albert <albert10130@proton.me>
2025-03-16 10:42:12 -0700

    finished

---
+++ backend/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+const axios = require("axios")
+
+const getTopCryptos = async () => {
+    try{
+        const response = await axios.get('https://api.coingecko.com/api/v3/coins/markets',
+            {
+                params: {
+                    vs_currency: 'usd',
+                    order: 'market_cap_desc',
+                    per_page: 20,
+                    page: 1,
+                    sparkline: false,
+                },
+            });
+        
+        const topCryptos = response.data;
+
+        console.log('Top 20 Cryptocurrencies: ');
+        topCryptos.forEach((crypto, index) => {
+            console.log(`${index + 1}.${crypto.name} (${crypto.symbol.toUpperCase()}):$${crypto.current_price}`);
+        });
+        return topCryptos;
+        
+    } catch (error) {
+    console.error('Error fetching data from CoinGecko: ', error);
+    }
+};
+
+app.use(express.static('./public'))
+
+app.get('/gettopcryptos', async (req, res) => {
+    const response = await getTopCryptos();
+    res.json(response);
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
---
+++ frontend/src/App.js
@@ -0,0 +1,77 @@
+import './App.css';
+import { useEffect, useState } from 'react';
+import "bootstrap-icons/font/bootstrap-icons.min.css";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// const axios = require('axios');
+
+
+function App() {
+  const [cryptos, setCryptos] = useState([]);
+  const [pattern, setPattern] = useState("");
+  const [sortitem, setSortitem] = useState(0);
+
+  const getdata = async () => {
+    const response = await axios.get('/gettopcryptos');
+    console.log(response.data);
+    setCryptos(response.data);
+  }
+
+  useEffect(() => {
+    getdata();
+  }, []);
+
+  const sort = (a, b) => {
+    switch(sortitem)
+    {
+      case 0:
+        return a.name < b.name ? 1 : -1;
+      case 1:
+        return a.current_price > b.current_price ? 1 : -1;
+      case 2:
+        return a.market_cap > b.market_cap ? 1 : -1;
+      case 3:
+        return a.total_volume > b.total_volume ? 1 : -1;
+      default:
+        return 1;
+    }
+  }
+
+  return (
+    <div className="App">
+      <input type="text" class="form-control" placeholder="Search" aria-label="Username" aria-describedby="basic-addon1" onChange={(e)=>{
+        setPattern(e.target.value);
+      }}/>
+      <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">No</th>
+        <th scope="col">Name<button class="btn btn-light" type="submit" onClick={e => setSortitem(0)}><i class="bi bi-arrow-down"></i></button></th>
+        <th scope="col">Price<button class="btn btn-light" type="submit" onClick={e => setSortitem(1)}><i class="bi bi-arrow-down"></i></button></th>
+        <th scope="col">Market Cap<button class="btn btn-light" type="submit" onClick={e => setSortitem(2)}><i class="bi bi-arrow-down"></i></button></th>
+        <th scope="col">24h Trading Volume<button class="btn btn-light" type="submit" onClick={e => setSortitem(3)}><i class="bi bi-arrow-down"></i></button></th>
+      </tr>
+    </thead>
+    <tbody>
+      {
+        cryptos.filter((item) => {
+          return item.name.indexOf(pattern) != -1
+        }).sort((a, b) => sort(b, a))
+        .map((item, index) => (
+          <tr className=''>
+            <th>{index}</th>
+            <td>{item.name}</td>
+            <td>{item.current_price}</td>
+            <td>{item.market_cap}</td>
+            <td>{item.total_volume}</td>
+          </tr>
+        ))
+      }
+      </tbody>
+  </table>
+    </div>
+  );
+}
+
+export default App;
